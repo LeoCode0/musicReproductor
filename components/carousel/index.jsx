81d249e32f7543264a54aeabf6ed1c01b8933c1e
Leonardo Espinoza <58142872+LeoCode0@users.noreply.github.com>
2021-03-06 13:05:14 -0600

    Carousel component

--- components/carousel/index.jsx
+++ components/carousel/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { FaPlay } from "react-icons/fa";
+import styles from "./Carousel.module.css";
 
 const CarouselItem = ({ cover, song, songName, album }) => {
   const handleClick = () => {
@@ -7,16 +8,20 @@ const CarouselItem = ({ cover, song, songName, album }) => {
   };
 
   return (
-    <li>
-      <div>
+    <li className={styles.list__item}>
+      <div className={styles.item__image}>
         <img src={cover} alt={songName} />
-        <button type="button" onClick={handleClick}>
+        <button
+          type="button"
+          onClick={handleClick}
+          className={styles.item__button}
+        >
           <FaPlay />
         </button>
       </div>
-      <div>
-        <p>{songName}</p>
-        <p>{album}</p>
+      <div className={styles.item__description}>
+        <p className={styles.song_name}>{songName}</p>
+        <p className={styles.song_artist}>{album}</p>
       </div>
     </li>
   );
@@ -24,20 +29,59 @@ const CarouselItem = ({ cover, song, songName, album }) => {
 
 export const Carousel = () => {
   const songs = [
-    { cover: "test", song: "test", songName: "test", album: "test", key: "1" },
+    {
+      cover: "https://picsum.photos/300",
+      song: "test",
+      songName: "test",
+      album: "test",
+      key: "1",
+    },
+    {
+      cover: "https://picsum.photos/300",
+      song: "test",
+      songName: "test",
+      album: "test",
+      key: "1",
+    },
+    {
+      cover: "https://picsum.photos/300",
+      song: "test",
+      songName: "test",
+      album: "test",
+      key: "1",
+    },
+    {
+      cover: "https://picsum.photos/300",
+      song: "test",
+      songName: "test",
+      album: "test",
+      key: "1",
+    },
+    {
+      cover: "https://picsum.photos/300",
+      song: "test",
+      songName: "test",
+      album: "test",
+      key: "1",
+    },
   ];
 
   useEffect(() => {
-    fetch("https://api.deezer.com/search?q=eminem").then((data) =>
-      console.log(data)
-    );
+    fetch(
+      `https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=paulo_londra&api_key=${process.env.API_KEY}&format=json`
+    )
+      .then((data) => data.json())
+      .then((json) => console.log(json));
   }, []);
 
   return (
-    <ul>
-      {songs.map((song) => (
-        <CarouselItem {...song} />
-      ))}
-    </ul>
+    <div>
+      <h2 className={styles.title}>TEXTO PARA MOSTRAR</h2>
+      <ul className={styles.list}>
+        {songs.map((song) => (
+          <CarouselItem {...song} />
+        ))}
+      </ul>
+    </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,21 @@ import {
   SearchContext,
   initialState as searchState,
 } from "../context/searchContext";
-import { SearchBar } from "../components/SearchBar/";
+import { SearchBar } from "../components/SearchBar";
 import { SongContext } from "../context/songContext";
-import { Carousel } from "../components/carousel/";
+import { Carousel } from "../components/carousel";
 import { initialState } from "../initialState";
+import { useEffect } from "react";
 
 function home() {
+  useEffect(() => {}, []);
   return (
     <SongContext.Provider value={initialState}>
       <SearchContext.Provider value={searchState}>
         <SearchBar />
         <Carousel />
       </SearchContext.Provider>
+      <Carousel />
     </SongContext.Provider>
   );
 }
